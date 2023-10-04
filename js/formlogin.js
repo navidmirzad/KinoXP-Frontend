a91fad784fb5e5e1f09bba6b700b38692499b34b
Enes Kocer <enes0539@stud.kea.dk>
2023-10-04 10:25:37 +0200

    login connects to backend

--- js/formlogin.js
+++ js/formlogin.js
@@ -1,1 +1,34 @@
 
+import {postObjectAsJson} from "./modulejson.js";
+
+document.addEventListener('DOMContentLoaded', createFormEventListener);
+let loginForm;
+
+function createFormEventListener() {
+    loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", handleFormSubmit);
+}
+
+async function handleFormSubmit(event) {
+    //Vi handler submitten her i stedet for default html behaviour
+    event.preventDefault()
+    const form = event.currentTarget;
+    const url = form.action;
+    console.log(form);
+    console.log(url);
+    try {
+        const formData = new FormData(form);
+        console.log(formData);
+        const responseData = await postFormDataAsJson(url, formData);
+    } catch (error) {
+        alert(error.message);
+        console.error(error);
+    }
+}
+
+async function postFormDataAsJson(url, formData) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    console.log(plainFormData)
+    const response = postObjectAsJson(url, plainFormData, "POST")
+    return response
+}
---
+++ js/modulejson.js
@@ -0,0 +1,15 @@
+async function postObjectAsJson(url, object, httpVerbum) {
+    const objectAsJsonString = JSON.stringify(object)
+    console.log(objectAsJsonString)
+    const fetchOptions = {
+        method: httpVerbum,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: objectAsJsonString
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+export {postObjectAsJson}
