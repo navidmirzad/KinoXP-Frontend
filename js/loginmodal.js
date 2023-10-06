f12850bee6aa11088f45ac516b1bcc9b1fa25247
navidmirzad <navi0221@stud.kea.dk>
2023-10-06 12:16:24 +0200

    AdminLoginPage html / css / login / sort of works. Needs to add authentication

--- js/loginmodal.js
+++ js/loginmodal.js
@@ -7,10 +7,8 @@ function closeLoginModal() {
     var modal = document.getElementById("loginModal");
     modal.style.display = "none"
 }
-
-// Handle form submission and authentication
-document.getElementById("loginForm").addEventListener("submit", function(event)
-{
-    event.preventDefault();
+// // Handle form submission and authentication (similar to the previous example)
+// document.getElementById("loginForm").addEventListener("submit", function(event) {
+//     event.preventDefault();
 //     // Your authentication logic here
-})
+// })
