27557f0cc80c000ef984039e46eeeaa69e98457c
math92n4 <math92n4@stud.kea.dk>
2023-10-11 13:46:35 +0200

    ordered tickets / session

--- js/frontpage.js
+++ js/frontpage.js
@@ -43,9 +43,52 @@ function myFunction() {
     }
 }*/
 
+const url = "http://localhost:8080/kinoxp/tickets"
+
 function openTicketsModal() {
     var modal = document.getElementById("ticketsModal");
     modal.style.display = "block";
+    const userName = sessionStorage.getItem("userName")
+    console.log(userName)
+    const newUrl = url + "/" + userName
+    console.log(newUrl)
+
+    fetch(newUrl).then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const tableBody = document.getElementById("ticketList")
+            tableBody.innerHTML = ""
+
+            data.forEach(ticket => {
+                const row = document.createElement("tr")
+
+                const date = document.createElement("td")
+                date.textContent = ticket.show.date
+                const time = document.createElement("td")
+                time.textContent = ticket.show.time
+                const title = document.createElement("td")
+                title.textContent = ticket.show.movie.title
+
+                const image = document.createElement("td")
+                const imageTag = document.createElement("img");
+                imageTag.src = ticket.show.movie.image;
+                image.appendChild(imageTag);
+
+                const theaterHall = document.createElement("td")
+                theaterHall.textContent = ticket.seat.theaterHall.name
+                const seat = document.createElement("td")
+                seat.textContent = ticket.seat.id
+
+                row.appendChild(date)
+                row.appendChild(time)
+                row.appendChild(title)
+                row.appendChild(image)
+                row.appendChild(theaterHall)
+                row.appendChild(seat)
+
+                tableBody.appendChild(row)
+            })
+        })
 }
 
 function closeTicketsModal() {
@@ -53,7 +96,4 @@ function closeTicketsModal() {
     modal.style.display = "none";
 }
 
-function myTickets() {
-
-}
 
