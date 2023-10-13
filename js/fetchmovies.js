536440a05adc796cb82411a1eab4898884fe4ca0
math92n4 <math92n4@stud.kea.dk>
2023-10-13 14:51:15 +0200

    put tickets

--- js/fetchmovies.js
+++ js/fetchmovies.js
@@ -1,7 +1,7 @@
 import {fetchAnyUrl, postObjectAsJson} from "./modulejson.js";
 
 const urlBase = "http://localhost:8080/kinoxp";
-
+const postTicketUrl = "http://localhost:8080/kinoxp/posticket"
 
 async function insertMovieCards(movie) {
     const movieCardDiv = document.createElement("div")
@@ -43,18 +43,91 @@ async function insertMovieCards(movie) {
     movieCardDiv.appendChild(movieLinkWrapper);
 
     const showTimes = await fetchShowTimesForMovie(movie.id);
+
     showTimes.forEach(showTime => {
-        const theaterHallAndTime = document.createElement("p")
-        const theaterHallAndDate = document.createElement("p")
-        theaterHallAndTime.textContent = showTime.theaterHall.name + " | " + showTime.time + " | " + showTime.date
+        const theaterHallAndTime = document.createElement("button");
 
-        movieCardDiv.appendChild(theaterHallAndTime)
-        movieCardDiv.appendChild(theaterHallAndDate)
-    })
+        theaterHallAndTime.textContent = showTime.theaterHall.name + " | " + showTime.time + " | " + showTime.date;
+        movieCardDiv.appendChild(theaterHallAndTime);
 
+        theaterHallAndTime.addEventListener("click", async (e) => {
+            e.preventDefault()
+            const modalContent = document.getElementById('modalContent');
+
+            // Fetch tickets for the selected showtime
+            const tickets = await fetchTicketsForShowTime(showTime.id);
+
+            const dropdown = document.createElement("select")
+
+            let seatId;
+
+            // Create and populate dropdowns for each ticket
+            tickets.forEach(ticket => {
+                const option = document.createElement("option")
+                option.value = ticket.id
+                console.log(option.value)
+                option.text = ticket.seat.id
+                seatId = ticket.seat.id
+                dropdown.appendChild(option)
+            });
+
+            const button = document.createElement("button")
+            button.textContent = "Select Ticket"
+
+            modalContent.appendChild(dropdown)
+            modalContent.appendChild(button);
+
+            button.addEventListener("click", async () => {
+                const selectedTicketId = dropdown.value; // Get the selected ticket's value
+                seatId = dropdown.options[dropdown.selectedIndex].text
+
+                const userName = sessionStorage.getItem("userName")
+
+                const ticketObj = {
+                    ticketId: selectedTicketId,
+                    seatId: seatId,
+                    userName: userName
+                }
+                console.log(ticketObj)
+                const response = await postObjectAsJson(postTicketUrl, ticketObj, "PUT")
+                console.log(response.status)
+
+                if (response.ok) {
+                    alert("Ticket has been bought")
+                } else {
+                    const errorText = await response.text()
+                    console.log(errorText)
+                    alert("Something went wrong")
+                }
+            });
+
+            const modal = document.getElementById("modal-order");
+            modal.style.display = 'block';
+
+        });
+
+
+    });
+
+        // Add an event listener to close the modal
+        const closeModal = document.getElementById('closeModal');
+        closeModal.addEventListener('click', () => {
+            const modalContent = document.getElementById('modalContent');
+            modalContent.style.display = 'none';
+        });
 
     const movieContainer = document.querySelector('.movie-container');
     movieContainer.appendChild(movieCardDiv);
+}
+
+function ticketDropdown(ticket) {
+    const modal = document.getElementById("modalContent")
+    modal.innerHTML += `
+            <h3>Tickets</h3>
+            <select>
+                <option value="${ticket.seat.id}">${ticket.seat.id}</option>
+            </select>`
+    console.log(ticket.seat.id)
 }
 
 let movies = []
@@ -73,6 +146,13 @@ async function fetchShowTimesForMovie(movieId) {
     const response = await fetch(showtimesUrl);
     const showtimesData = await response.json();
     return showtimesData;
+}
+
+async function fetchTicketsForShowTime(showId) {
+    const url = "http://localhost:8080/kinoxp/tickets/byshow/" + showId
+    const response = await fetch(url)
+    const ticketData = await response.json()
+    return ticketData
 }
 
 document.addEventListener("DOMContentLoaded", actionGetMovies)
